56229eae73067cbdbeeab20b264a8db0fbc71d19
pruthi-58 <pruthi.gopani@swiftrut.com>
2024-12-11 00:35:34 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
+    credentials: true
 }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export{app}
+export { app }
